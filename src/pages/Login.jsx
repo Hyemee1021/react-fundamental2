3fd7f05cc827cf731bbf88d07d5a0f11dddec323
Hye Mee Park <hyemee1021@gmail.com>
2024-08-29 20:56:07 +1000

    using useReducer for state mangement

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,11 +4,15 @@ import { Navbar } from "../components/Navbar";
 function reducer(state, action) {
   switch (action.type) {
     case "password":
-      return { ...state, password: action.payload };
+      return {
+        ...state,
+        password: action.payload,
+      };
     case "checkPassword":
-      return { ...state, checkPassword: action.payload };
-    default:
-      throw new Error(" Unknown action");
+      return {
+        ...state,
+        checkPassword: action.payload,
+      };
   }
 }
 
@@ -20,29 +24,32 @@ const initialState = {
 export const Login = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const handlePasswordChange = (e) => {
-    dispatch({ type: "password", payload: e.target.value });
-  };
-  const handleCheckPasswordChange = (e) => {
-    dispatch({ type: "checkPassword", payload: e.target.value });
-  };
-  console.log(state);
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex flex-grow items-center justify-center mt-16">
         <form className="flex flex-col gap-5 w-3/4 max-w-md bg-blue-300 p-6 rounded-lg shadow-lg ">
           <input
-            onChange={handlePasswordChange}
             className="bg-slate-200 p-2 "
             type="password"
             placeholder="Enter your password"
+            onChange={(e) =>
+              dispatch({
+                type: "assword",
+                payload: e.target.value,
+              })
+            }
           />
           <input
-            onChange={handleCheckPasswordChange}
             type="password"
             className="bg-slate-200 p-2 "
             placeholder="Re-enter your password"
+            onChange={(e) =>
+              dispatch({
+                type: "checkPassword",
+                payload: e.target.value,
+              })
+            }
           />
           <button className="btn bg-slate-50 p-2 hover:bg-slate-200">
             {" "}
