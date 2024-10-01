d387c96726ce497f756fd5a049201098ec90e11f
Hye Mee Park <hyemee1021@gmail.com>
2024-10-01 21:06:01 +1000

    using api build a esarch bar

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,85 +1,43 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { Navbar } from "../components/Navbar";
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "password":
-      return {
-        ...state,
-        password: action.payload,
-      };
-    case "checkPassword":
-      return {
-        ...state,
-        checkPassword: action.payload,
-      };
-    case "submit":
-      return {
-        ...state,
-        match: state.password === state.checkPassword,
-      };
-  }
-}
-
-const initialState = {
-  password: "",
-  checkPassword: "",
-  match: false,
-};
 export const Login = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch({
-      type: "submit",
-    });
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState([]);
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    console.log(input);
   };
+
+  const fetchFun = () => {
+    if (input) {
+      const res = fetch(`https://api.datamuse.com/words?ml=${input}&max=5`)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setResults(data);
+        });
+    } else {
+      setResults([]);
+    }
+  };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      fetchFun();
+    }, 300);
+
+    return () => clearTimeout(timer);
+  }, [input]);
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex flex-col flex-grow items-center justify-center  gap-3 mt-16">
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col gap-5 w-3/4 max-w-md bg-blue-300 p-6 rounded-lg shadow-lg "
-        >
-          <input
-            className="bg-slate-200 p-2 "
-            type="password"
-            placeholder="Enter your password"
-            onChange={(e) =>
-              dispatch({
-                type: "password",
-                payload: e.target.value,
-              })
-            }
-          />
-          <input
-            type="password"
-            className="bg-slate-200 p-2 "
-            placeholder="Re-enter your password"
-            onChange={(e) =>
-              dispatch({
-                type: "checkPassword",
-                payload: e.target.value,
-              })
-            }
-          />
-          <button
-            className="btn bg-slate-50 p-2 hover:bg-slate-200"
-            type="submit"
-          >
-            {" "}
-            Submit
-          </button>
-        </form>
-        <div>
-          {state.match ? (
-            <p>Passwords matched</p>
-          ) : (
-            <p>Passwords not matched</p>
-          )}
-        </div>
+        <input className="border" type="text" onChange={handleChange} />
+        {results.map((result) => (
+          <p>{result.word}</p>
+        ))}
       </main>
     </div>
   );
